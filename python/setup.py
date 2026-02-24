71e33800fd46ab56b2f2fa84423e522382cd0fdb
Ksenia <briling@mail.ru>
2026-02-24 09:43:13 +0100

    Fix build and installation

--- python/setup.py
+++ python/setup.py
@@ -3,8 +3,10 @@ import subprocess
 import os
 import tempfile
 import shutil
+from pathlib import Path
 
-VERSION="3.0rc1"
+
+VERSION="3.0rc2"
 
 def get_git_version_hash():
     """Get tag/hash of the latest commit.
@@ -19,35 +21,20 @@ def get_git_version_hash():
     if not version.strip():
         return VERSION + "+unknown"
     print(version)
-    return version.strip().decode().replace('-', '+', 1)
-
-
-def check_compilation():
-    """Check if C compiler and X libraries are available.
-
-    Thanks to https://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script
-    """
-    omp_test = 'void main() { }'
-    tmpdir = tempfile.mkdtemp()
-    curdir = os.getcwd()
-    os.chdir(tmpdir)
-    filename = r'test.c'
-    with open(filename, 'w') as file:
-        file.write(omp_test)
-    with open(os.devnull, 'w') as fnull:
-        result = subprocess.call(['cc', '-lX11', '-lXpm', filename], stdout=fnull, stderr=fnull)
-    os.chdir(curdir)
-    shutil.rmtree(tmpdir)
-    return not result
+    return version.strip().decode().replace('+', '-').replace('-', '+', 1)
 
 
 if __name__ == '__main__':
-    # TODO
-    #check_compilation()
 
-    all_c_files = [os.path.join(root, file) for root, dirs, files in os.walk('../src') for file in files if file.endswith('.c')]
+    setup_dir = Path(__file__).parent
+    src_dir = setup_dir.parent / "src"
 
-    INCL=['-I../src/'+i for i in ['mol', 'math', 'v', 'sym']]
+    def rel_posix(path):
+        return os.path.relpath(path, start=setup_dir).replace(os.sep, "/")
+
+    c_files = [rel_posix(p) for p in src_dir.rglob("*.c")]
+    include_dirs = sorted({ rel_posix(h.parent) for h in src_dir.rglob("*.h") })
+
     try:
         GIT_HASH = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode()
         GIT_BRANCH = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode()
@@ -56,14 +43,16 @@ if __name__ == '__main__':
         GIT_BRANCH = "unknown"
     VERSION_FLAGS = [f'-DGIT_HASH="{GIT_HASH}"',
                      f'-DGIT_BRANCH="{GIT_BRANCH}"',
-                     f'-DBUILD_USER="{os.getenv('USER')}@{os.getenv('HOSTNAME')}"',
+                     f'-DBUILD_USER="{os.getenv("USER")}@{os.getenv("HOSTNAME")}"',
                      f'-DBUILD_DIRECTORY="{os.getcwd()}"']
 
     setup(
         version=get_git_version_hash(),
         ext_modules=[Extension('vmol.v',
-                               all_c_files,
-                               extra_compile_args=['-std=gnu11', '-O2', ] + INCL + VERSION_FLAGS,
-                               extra_link_args=['-lX11', '-lXpm']),
+                               sources=c_files,
+                               include_dirs=include_dirs,
+                               libraries = ['X11', 'Xpm'],
+                               extra_compile_args=['-std=gnu11', '-O2', ] + VERSION_FLAGS,
+                               extra_link_args=[]),
                     ],
     )
