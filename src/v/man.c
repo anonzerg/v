c5f21526679399ac258d9c13293d22ca06468124
Ksenia <briling@mail.ru>
2026-03-20 21:15:50 +0100

    Update readme and man wrt d864910

--- src/v/man.c
+++ src/v/man.c
@@ -9,31 +9,30 @@ void printman(FILE * f, char * exename){
   \n\
  OPTIONS:\n\
   \n\
-  vib:%%d                                       force to show geometries (0) / vibrations (1) \n\
-  bonds:0                                      disable bonds\n\
-  bmax:%%lf                                     max. length of a bond to display\n\
-  bohr:1                                       assume input files are in Bohr (default is Å)\n\
-  dt:%%lf                                       delay between frames in seconds (default %g)\n\
-  symtol:%%lf                                   tolerance for symmetry determination in Å (default %g) \n\
-  z:%%d,%%d,%%d,%%d,%%d                             show an internal coordinate: \n\
-                                                     1,i,j,0,0 - distance i-j\n\
-                                                     2,i,j,k,0 - angle    i-j-k\n\
-                                                     3,i,j,k,l - torsion  i-j-k-l\n\
-  rot:%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf      rotation matrix to start with (default identity matrix)\n\
-  frame:%%d                                     frame to start with (default 1)\n\
-  font:%%s                                      font (xlfd) \n\
-  colors:%%s                                    colorscheme (\"v\" (default) or \"cpk\") \n\n\
-  cell:b%%lf,%%lf,%%lf                            cuboid size in a.u. \n\
-  cell:%%lf,%%lf,%%lf                             cuboid size in Å \n\
-  cell:b%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf    cell parameters in a.u. \n\
-  cell:%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf     cell parameters in Å \n\
-  shell:b%%lf,%%lf                               spheres radii in a.u. \n\
-  shell:%%lf,%%lf                                spheres radii in Å \n\
-  center:%%d                                    origin is geometric center (1, default) / center of mass (2) / as is (0) \n\
-  inertia:%%d                                   if rotate molecules wrt axis of inertia (1) or not (0, default) \n\n\
-  gui:%%d                                       normal (1) / headless (0) mode \n\
-  com:%%s                                       command sequence for gui:0 \n\
-  exitcom:%%s                                   command sequence to run on exit (same as for gui:0) \n\
+  vib:%%d                                         force to show geometries (0) / vibrations (1) \n\
+  bonds:0                                        disable bonds\n\
+  bmax:%%lf                                       max. length of a bond to display\n\
+  bohr:1                                         assume input files are in Bohr (default is Å)\n\
+  dt:%%lf                                         delay between frames in seconds (default %g)\n\
+  symtol:%%lf                                     tolerance for symmetry determination in Å (default %g) \n\
+  z:%%d,%%d,%%d,%%d,%%d                               show an internal coordinate: \n\
+                                                       1,i,j,0,0 - distance i-j\n\
+                                                       2,i,j,k,0 - angle    i-j-k\n\
+                                                       3,i,j,k,l - torsion  i-j-k-l\n\
+  rot:%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf        rotation matrix to start with (default identity matrix)\n\
+  frame:%%d                                       frame to start with (default 1)\n\
+  font:%%s                                        font (xlfd) \n\
+  colors:%%s                                      colorscheme (\"v\" (default) or \"cpk\") \n\n\
+  cell:0                                         disable PBC from the extended xyz file header \n\
+  cell:b%%lf[,%%lf,%%lf[,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf]]  cubic / orthogonal / non-orhogonal cell parameters in a.u. \n\
+  cell:%%lf[,%%lf,%%lf[,%%lf,%%lf,%%lf,%%lf,%%lf,%%lf]]   cubic / orthogonal / non-orhogonal cell parameters in Å \n\
+  shell:b%%lf[,%%lf]                               sphere(s) radii in a.u. \n\
+  shell:%%lf[,%%lf]                                sphere(s) radii in Å \n\
+  center:%%d                                      origin is geometric center (1, default) / center of mass (2) / as is (0) \n\
+  inertia:%%d                                     if rotate molecules wrt axis of inertia (1) or not (0, default) \n\n\
+  gui:%%d                                         normal (1, default) / headless (0) mode \n\
+  com:%%s                                         command sequence for gui:0 \n\
+  exitcom:%%s                                     command sequence to run on exit (same as for gui:0) \n\
   \n\
  KEYBOARD REFERENCE:\n\
   \n\
@@ -44,7 +43,8 @@ void printman(FILE * f, char * exename){
   0                        go to the first point \n\
   =                        go to the last point  \n\
   enter/backspace          next/previous point   \n\
-  ins                      play forwards  / stop  (vibration mode: animate selected normal mode / stop)\n\
+  ins                      play forwards  / stop \n\
+                               vibration mode: animate selected normal mode / stop\n\
   del                      play backwards / stop \n\
   \n\
   home/end                 zoom in/out           \n\
@@ -63,9 +63,10 @@ void printman(FILE * f, char * exename){
   x                        print molecule (Priroda input + bonds)    \n\
   z                        print molecule (.xyz)    \n\
   p                        print molecule (input for an .svg generator)    \n\
-  u                        print current rotation matrix       \n\
+  u                        print the current rotation matrix       \n\
   m                        save the current frame (.xpm format)\n\
-  f                        save all frames (vibration mode: save all frames to animate the selected normal mode)\n\
+  f                        save all frames starting from the current one \n\
+                               vibration mode: save all frames to animate the selected normal mode) \n\
   \n\
   j                        jump to a frame (will be prompted)\n\
   \n\
